6300d075959a5c8dd65e776665281ea1648b5d55
willAtaides <sg552.santos@gmail.com>
2025-07-06 09:17:42 -0300

    Categoria - iniciando projeto

---
+++ src/main/java/com/devprogram/eventosystem/EventosystemApplication.java
@@ -0,0 +1,13 @@
+package com.devprogram.eventosystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventosystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventosystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devprogram/eventosystem/EventosystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devprogram.eventosystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventosystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
